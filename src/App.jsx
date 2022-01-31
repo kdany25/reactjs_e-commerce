89cccb29536dab5dbb112f7af26d9742cf32b52c
kdany25 <kabadany25@gmail.com>
2022-01-31 12:06:32 +0200

    bug-fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import Product from "./pages/Product"
-import Home from "./pages/Home"
-import ProductList from "./pages/ProductList"
-import SignUp from "./pages/SignUp"
-import Login from './pages/Login'
-import Cart from "./pages/Cart"
-const App = () =>{
-    return <div>
-        <Cart/>
-    </div>
-}
-export default App
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Switch, Route , Redirect} from "react-router-dom";
+const App = () => {
+    const user = true
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products">
+          <ProductList />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <SignUp />}
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+export default App;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,19 +1,24 @@
 import styled from "styled-components";
-import Announcement from "../components/Announcement";
-import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import Newsletter from "../components/Newsletter";
+import Announcement from "../components/Announcement";
 import Products from "../components/Products";
-import {mobile} from "../responsive";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import { mobile } from "../responsive";
+import { useLocation } from "react-router";
+import { useState } from "react";
 
 const Container = styled.div``;
+
 const Title = styled.h1`
   margin: 20px;
 `;
+
 const FilterContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;
+
 const Filter = styled.div`
   margin: 20px;
   ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
@@ -25,52 +30,64 @@ const FilterText = styled.span`
   margin-right: 20px;
   ${mobile({ marginRight: "0px" })}
 `;
-const Select = styled.select`
-padding: 10px;
-margin-right: 20px;
-${mobile({ margin: "10px 0px" })}
 
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
 `;
 const Option = styled.option``;
+
 const ProductList = () => {
+  const location = useLocation();
+  const cat = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState("newest");
+
+  const handleFilters = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filters,
+      [e.target.name]: value,
+    });
+  };
+
   return (
     <Container>
-      <Announcement />
       <Navbar />
-      <Title>Dress</Title>
+      <Announcement />
+      <Title>{cat}</Title>
       <FilterContainer>
         <Filter>
-          <FilterText>Filter Product</FilterText>
-          <Select>
-            <Option disabled selected>
-              Color
-            </Option>
-            <Option>White</Option>
-            <Option>Black</Option>
-            <Option>Red</Option>
-            <Option>Green</Option>
+          <FilterText>Filter Products:</FilterText>
+          <Select name="color" onChange={handleFilters}>
+            <Option disabled>Color</Option>
+            <Option>white</Option>
+            <Option>black</Option>
+            <Option>red</Option>
+            <Option>blue</Option>
+            <Option>yellow</Option>
+            <Option>green</Option>
           </Select>
-          <Select>
-            <Option disabled selected>
-              Size
-            </Option>
-            <Option>Xs</Option>
+          <Select name="size" onChange={handleFilters}>
+            <Option disabled>Size</Option>
+            <Option>XS</Option>
             <Option>S</Option>
             <Option>M</Option>
             <Option>L</Option>
-            <Option>ML</Option>
+            <Option>XL</Option>
           </Select>
         </Filter>
         <Filter>
-          <FilterText>Sort Product:</FilterText>
-          <Select>
-            <Option selected>Newest</Option>
-            <Option>Price (asc)</Option>
-            <Option>Price(desc)</Option>
+          <FilterText>Sort Products:</FilterText>
+          <Select onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
-      <Products />
+      <Products cat={cat} filters={filters} sort={sort} />
       <Newsletter />
       <Footer />
     </Container>
