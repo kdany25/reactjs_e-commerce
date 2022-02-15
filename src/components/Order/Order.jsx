f6ea9e043bed9803f0f844cb4ce6247593bac933
kdany25 <kabadany25@gmail.com>
2022-02-15 15:32:28 +0200

    ft:(order)

---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import "./order.css";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addOrder } from "../../redux/apiCalls";
+import { useSelector } from "react-redux";
+
+export default function Order({ setOpenModal ,cart  }) {
+  const user = useSelector((state) => state.user.currentUser);
+
+console.log(cart.products[0])
+  const [inputs, setInputs] = useState({});
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const order = { ...inputs , products: cart.products , user , amount : cart.total };
+    addOrder(order, dispatch);
+  }
+    return (
+      <div className="modalBackground">
+        <div className="modalContainer">
+          <div className="titleCloseBtn">
+            <button
+              onClick={() => {
+                setOpenModal(false);
+              }}
+            >
+              {" "}
+              X
+            </button>
+          </div>
+          <div className="newUser">
+            <h1 className="newUserTitle">New Patient</h1>
+            <form className="newUserForm">
+           
+              <div className="newUserItem">
+                <label>Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="john@gmail.com"
+               
+                />
+              </div>
+
+              <div className="newUserItem">
+                <label>Phone</label>
+                <input
+                  name="phone"
+                  type="text"
+                  placeholder="+250788730199"
+                  
+                />
+              </div>
+              <div className="newUserItem">
+                <label>Address</label>
+                <input
+                  name="address"
+                  type="text"
+                  placeholder="Kigali | Rwanda"
+                  onChange={handleChange}
+                />
+              </div>
+            </form>
+          </div>
+
+          <div className="footer">
+            <button
+              onClick={() => {
+                setOpenModal(false);
+              }}
+              id="cancelBtn"
+            >
+              Cancel
+            </button>
+            <button onClick={handleClick}>Continue</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -13,6 +13,21 @@ import {  getSubscriberStart,
   addSubscriberSuccess,
   addSubscriberFailure,} from "./subRedux"
 
+  import {
+    getOrderStart,
+    getOrderSuccess,
+    getOrderFailure,
+    deleteOrderStart,
+    deleteOrderSuccess,
+    deleteOrderFailure,
+    updateOrderStart,
+    updateOrderSuccess,
+    updateOrderFailure,
+    addOrderStart,
+    addOrderSuccess,
+    addOrderFailure,
+  } from "./orderRedux"
+
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
@@ -62,5 +77,35 @@ export const addSub = async (subscriber, dispatch) => {
     dispatch(addSubscriberSuccess(res.data));
   } catch (err) {
     dispatch(addSubscriberFailure());
+  }
+};
+
+
+export const deleteOrder = async (id, dispatch) => {
+  dispatch(deleteOrderStart());
+  try {
+    const res = await userRequest.delete(`/order/${id}`);
+    dispatch(deleteOrderSuccess(res.data));
+  } catch (err) {
+    dispatch(deleteOrderFailure());
+  }
+};
+
+export const updateOrder = async (id, order, dispatch) => {
+  dispatch(updateOrderStart());
+  try {
+    // update
+    dispatch(updateOrderSuccess({ id, order }));
+  } catch (err) {
+    dispatch(updateOrderFailure());
+  }
+};
+export const addOrder = async (order, dispatch) => {
+  dispatch(addOrderStart());
+  try {
+    const res = await userRequest.post(`/order`, order);
+    dispatch(addOrderSuccess(res.data));
+  } catch (err) {
+    dispatch(addOrderFailure());
   }
 };
