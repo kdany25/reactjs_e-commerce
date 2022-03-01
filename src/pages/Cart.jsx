086d280d9232525bc748354759c66e5c8053d152
kdany25 <kabadany25@gmail.com>
2022-03-01 16:31:10 +0200

    ft:(redux-persist)

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -176,14 +176,36 @@ const Cart = () => {
         <Navbar />
         <Wrapper>
           <Title>CART</Title>
-          <Top>
+          {/* <Top>
             <Link to="/products" style={{ color: "black" }}>
               <TopButton>CONTINUE SHOPPING</TopButton>
             </Link>
-          </Top>
+          </Top> */}
           <Bottom>
             <Info>
-              {cart.products.map((product, index) => (
+              {cart.products.length === 0 ? (
+        <div className="cart-empty">
+          <p>Your cart is currently empty</p>
+          <div className="start-shopping">
+            <Link to="/products">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-arrow-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                />
+              </svg>
+              <span>Start Shopping</span>
+            </Link>
+          </div>
+        </div>
+      ) :cart.products.map((product, index) => (
                 <Product key={index}>
                   <ProductDetail>
                     <Image src={product.img} />
